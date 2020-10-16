fb2ead2d7035f8524555cdcc9f2620e4102a4dd1
Dimitri Snijder <dimitri@ivaldi.nl>
2020-10-16 15:31:56 +0200

    Update

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -56,6 +56,44 @@ module Spree::OrderDecorator
   def has_order_adjustments?
     order_adjustment_total.abs > 0
   end
+
+  def update_from_params(params, permitted_params, request_env = {})
+    success = false
+    @updating_params = params
+
+    # Set existing card after setting permitted parameters because
+    # rails would slice parameters containg ruby objects, apparently
+    existing_card_id = @updating_params[:order] ? @updating_params[:order].delete(:existing_card) : nil
+
+    attributes = if @updating_params[:order]
+                    @updating_params[:order].permit(permitted_params).delete_if { |_k, v| v.nil? }
+                  else
+                    {}
+                  end
+
+    if existing_card_id.present?
+      credit_card = CreditCard.find existing_card_id
+      if credit_card.user_id != user_id || credit_card.user_id.blank?
+        raise Core::GatewayError, Spree.t(:invalid_credit_card)
+      end
+
+      credit_card.verification_value = params[:cvc_confirm] if params[:cvc_confirm].present?
+
+      attributes[:payments_attributes].first[:source] = credit_card
+      attributes[:payments_attributes].first[:payment_method_id] = credit_card.payment_method_id
+      attributes[:payments_attributes].first.delete :source_attributes
+    end
+
+    if attributes[:payments_attributes]
+      attributes[:payments_attributes].first[:request_env] = request_env
+    end
+
+    success = update(attributes)
+    set_shipments_cost if shipments.any?
+
+    @updating_params = nil
+    success
+  end
 end
 
 Spree::Order.prepend(Spree::OrderDecorator)
